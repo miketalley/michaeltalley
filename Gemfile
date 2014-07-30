e563d35e1013be112b1c7713538dec3f8d82ca49
Mike Talley <miketalley@hotmail.com>
2014-07-30 14:32:54 -0400

    Changed resume route on nav

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+# gem 'pry-rails'
 gem 'newrelic_rpm'
 gem 'angularjs-rails'
 gem 'puma'
--- app/mailers/resume_mailer.rb
+++ app/mailers/resume_mailer.rb
@@ -2,6 +2,7 @@ class ResumeMailer < ActionMailer::Base
   default from: "michael.d.talley@gmail.com"
 
   def email_resume(email)
+    # attachments['MikeTalleyResume.pdf'] = File.read('../assets/MikeTalleyResume.pdf')
     mail(to: email, subject: "Mike Talley's Resume")
   end
 end
